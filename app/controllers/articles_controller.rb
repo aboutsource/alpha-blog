ed19fb661bdd99c6860c398b95b252d8c4807060
victoria-hodder-as <victoria.hodder@aboutsource.net>
2021-01-26 15:05:35 +0100

    add article edit and update feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         # :article = key for displaying title and description from article object
@@ -22,6 +26,16 @@ class ArticlesController < ApplicationController
             render 'new'
         # code to show action path
         end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Yay, you made your post even more beautiful"
+            redirect_to @article
+        else
+            render 'edit'
+        end 
     end
 
 end
