3e4deed07064c107562f6e63d3bfa06b44803bdd
victoria-hodder-as <victoria.hodder@aboutsource.net>
2021-01-29 16:55:42 +0100

    add notes in review

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
+# To disable extra div when you hit a validation error message
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   html_tag.html_safe
 end
