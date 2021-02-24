a77d410d342af6769458d37a4279c3acc24f0e21
victoria-hodder-as <victoria.hodder@aboutsource.net>
2021-02-24 14:16:07 +0100

    add category to setup to add category_ids to article params

--- test/integration/create_article_test.rb
+++ test/integration/create_article_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class CreateArticleTest < ActionDispatch::IntegrationTest
   setup do
+    @category = Category.create(name: "Travel", id: 1)
     @test_user = User.create(username: "Victoria", email: "victoria@email.com",
                               password: "password", admin: false)
     sign_in_as(@test_user)
@@ -12,7 +13,7 @@ class CreateArticleTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_difference 'Article.count', 1 do
       post articles_path, params: {article: { title: "Nice title", description: "Nice description", 
-                                  category_ids: [] } } # fails if I put categories inside - why?
+                                  category_ids: [1] } }
     end
     assert_response :redirect
     assert_redirected_to article_url(Article.last)
