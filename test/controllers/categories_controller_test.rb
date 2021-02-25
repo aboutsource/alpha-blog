e1eae5c586b28306811a0d8e8c99fbe9c975544a
victoria-hodder-as <victoria.hodder@aboutsource.net>
2021-02-25 14:29:17 +0100

    add to categories_controller_test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @category = Category.create(name: "Sports")
+    @category = Category.create(id: 1, name: "Sports")
     @admin_user = User.create(username: "vhodder", email: "vhodder@email.com",
                               password: "password", admin: true)
   end
@@ -37,9 +37,11 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to categories_url
   end
 
-  test "should show category" do
+  test "should show articles listing in category page" do
     get category_url(@category)
     assert_response :success
+    assert_match "Articles", response.body
+    assert_match "Category: #{@category.name}", response.body
   end
 
 end
