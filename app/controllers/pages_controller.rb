5c69d787bb4bd9ec15bf0ef261410f2af9371635
victoria-hodder-as <victoria.hodder@aboutsource.net>
2021-02-26 15:53:57 +0100

    add simple search function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,17 @@ class PagesController < ApplicationController
   end
 
   def about
+    # TODO
   end
+
+  def search
+    if params[:search].blank?
+      flash[:alert] = "Search cannot be blank"
+      redirect_to root_path
+    else
+      @parameter = params[:search].downcase
+      @results = Article.all.where("lower(title) LIKE :search", search: "%#{@parameter}%")
+    end 
+  end
+
 end
