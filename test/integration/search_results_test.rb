7a2ce869b8a283563ff732c624fe914e4bcb0998
victoria-hodder-as <victoria.hodder@aboutsource.net>
2021-03-03 09:21:14 +0100

    add test for listing search results

--- test/integration/search_results_test.rb
+++ test/integration/search_results_test.rb
@@ -2,13 +2,18 @@ require "test_helper"
 
 class ListSearchResultsTest < ActionDispatch::IntegrationTest
   def setup
-    # Create articles for "should show search results" test
-    @article = Article.create(title: "A title", description: "This is great")
-    @article2 = Article.create(title: "Another title", description: "This is really great")
+    user = User.create!(username: "Victoria", email: "victoria@email.com", password: "secretpassword")
+    @article = Article.create!(title: "A title", description: "This is nice", user: user)
+    @article2 = Article.create!(title: "Another nice title", description: "This is really nice", user: user)
+    @article3 = Article.create!(title: "Another bad title", description: "This is really terrible", user: user)
   end
 
   test "should show search results" do
-    # TODO:
+    get '/search?search=nice'
+    assert_response :success
+    assert_match "Search Result", response.body
+    assert_select "a[href=?]", article_path(@article), text: @article.title
+    assert_select "a[href=?]", article_path(@article2), text: @article2.title
   end
 
   test "should show error if search bar empty" do
