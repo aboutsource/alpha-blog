1fc30df859d724d1c69641301b30db0a01609a8a
victoria-hodder-as <victoria.hodder@aboutsource.net>
2021-03-12 14:48:34 +0100

    replace has_many through association with has_and_belongs_to_many

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :article_categories
-  has_many :categories, through: :article_categories, dependent: :delete_all
+  has_and_belongs_to_many :categories, join_table: :article_categories
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 15 }
   validates_uniqueness_of :name
-  has_many :article_categories
-  has_many :articles, through: :article_categories, dependent: :delete_all
+  has_and_belongs_to_many :articles, join_table: :article_categories
 end
