593cb7e6387abe0eeec8e63a9c36eb98dafba564
victoria-hodder-as <victoria.hodder@aboutsource.net>
2021-03-15 16:48:42 +0100

    add integration test to check category is deleted

--- test/integration/delete_category_test.rb
+++ test/integration/delete_category_test.rb
@@ -3,13 +3,23 @@ require "test_helper"
 class DeleteCategoryTest < ActionDispatch::IntegrationTest
 
   setup do
+    @category = Category.create(name: "Sports")
     @admin_user = User.create(username: "vhodder", email: "vhodder@email.com",
                               password: "password", admin: true)
     sign_in_as(@admin_user)
   end
 
   test "should delete category" do
-    #TODO
+    get "/categories/1"
+    assert_response :success
+    assert_match "Are you sure?", response.body
+    assert_difference 'Category.count', -1 do
+      delete category_path, params: { category: { name: "Sports" } }
+    end
+    assert_response :redirect
+    assert_redirected_to categories_url
+    follow_redirect!
+    assert_match "Categories", response.body
   end
 
   test "should not delete associated article" do
@@ -17,3 +27,4 @@ class DeleteCategoryTest < ActionDispatch::IntegrationTest
   end
 
 end
+
