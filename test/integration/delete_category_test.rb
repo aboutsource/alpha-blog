4c67526c17e7a59bf0123070ba3bd00cf5de2f26
victoria-hodder-as <victoria.hodder@aboutsource.net>
2021-03-16 13:12:13 +0100

    remove unnecessary tests

--- test/integration/delete_category_test.rb
+++ test/integration/delete_category_test.rb
@@ -8,32 +8,11 @@ class DeleteCategoryTest < ActionDispatch::IntegrationTest
     sign_in_as(admin_user)
     @category = Category.create!(name: "Sports")
     @article = Article.create!(title: "Rugby is great", description: "This is a great sport", 
-                                user: admin_user, category_ids: [1])
+                                user: admin_user, category_ids: [@category.id])
   end
 
-  test "should delete category" do
-    get "/categories/1"
-    assert_response :success
-    assert_match "Are you sure?", response.body
-    assert_difference 'Category.count', -1 do
-      delete category_path, params: { category: { name: "Sports" } }
-    end
-    assert_response :redirect
-    assert_redirected_to categories_url
-    follow_redirect!
-    assert_match "Categories", response.body
-  end
 
   test "should not delete associated article" do
-    # Check article show page
-    get "/articles/1"
-    assert_response :success
-    # Check the title is there
-    assert_match "Rugby is great", response.body
-    # Check the description is there
-    assert_match "This is a great sport", response.body
-    # Check category button is in body
-    assert_match "Sports", response.body
 
     # Delete the category
     get "/categories/1"
