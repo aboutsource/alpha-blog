8d9e29035ae7c8e5d4314ff23d0da22c6ce66a42
victoria-hodder-as <victoria.hodder@aboutsource.net>
2021-04-13 12:24:25 +0200

    remove unnecessary if block in confirmation_token method

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,9 +21,7 @@ class User < ApplicationRecord
 
   private
   def confirmation_token
-    if self.confirm_token.blank?
-      self.confirm_token = SecureRandom.urlsafe_base64.to_s
-    end
+    self.confirm_token = SecureRandom.urlsafe_base64.to_s
   end
 
 end
